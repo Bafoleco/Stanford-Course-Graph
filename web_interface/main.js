60ab244176d7143559db736d91d71dd10b5b049c
Bay Foley-Cox <bayfoleycox@gmail.com>
2020-08-29 22:35:16 -0400

    added arrows

--- web_interface/main.js
+++ web_interface/main.js
@@ -1,7 +1,4 @@
 const vis = d3.select("#graph")
-            .append("svg")
-            .attr("width", 1500)
-            .attr("height", 1500);
 
 fetch("/graph.json")
     .then((res) => res.json())
@@ -12,14 +9,15 @@ fetch("/graph.json")
 
 function render(nodes, links) {
     const link = vis.selectAll(".line")
-   .data(links)
-   .enter()
-   .append("line")
-   .attr("x1", function(d) { return d.source.x })
-   .attr("y1", function(d) { return d.source.y })
-   .attr("x2", function(d) { return d.target.x })
-   .attr("y2", function(d) { return d.target.y })
-   .style("stroke", "rgb(6,120,155)");
+       .data(links)
+       .enter()
+       .append("line")
+       .attr("x1", d => d.source.x )
+       .attr("y1", d => d.source.y )
+       .attr("x2", d => d.target.x )
+       .attr("y2", d => d.target.y )
+       .attr("marker-end", "url(#arrow)")
+       .style("stroke", "rgb(6,120,155)");
 
     const node = vis.append("g")
         .attr("class", "nodes")
@@ -40,16 +38,16 @@ function render(nodes, links) {
         .nodes(nodes)
         .links(links)
         .size([1500, 1500])
-        .linkStrength(0.4)
-        .friction(0.9)
-        .linkDistance(25)
-        .charge(-90)
-        .gravity(0.07)
-        .theta(0.8)
+        .linkStrength(0.3)
+        .friction(0.90)
+        .linkDistance(30)
+        .charge(-150)
+        .gravity(0.06)
+        .theta(0.9)
         .alpha(0.3)
         .start();
 
-    for (var i = 0; i < 80; ++i) {
+    for (var i = 0; i < 100; ++i) {
         force.tick();
     }
 
